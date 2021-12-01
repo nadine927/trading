60fcbf6766219be40b4e3a2f76d446015e1d9fb9
Telesphore <tuganimana01@gmail.com>
2021-12-01 15:35:19 +0200

    added value

--- ussd/urls.py
+++ ussd/urls.py
@@ -6,5 +6,5 @@ from .views import *
 urlpatterns = [
     path('', views.welcome, name='welcome') ,
     path('ussd/', views.ussdApp, name='ussd')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
  
